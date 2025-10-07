430e4301be4d82ec9185ff36e138a2d9425efe95
Ashley Michel Cabrera Mena <amcabreramena@gmail.com>
2025-10-07 16:10:52 -0400

    Agregar hola.js y workflow completo

--- hola.js
+++ hola.js
@@ -1,1 +1,9 @@
-console.log("Hola Mundo");
+// hola.js
+const estudiante = "Ashley Cabrera Mena";
+const curso = "DevOps ITLA";
+const fecha = new Date().toLocaleString();
+
+console.log("Hola Mundo!");
+console.log("Estudiante:", estudiante);
+console.log("Curso:", curso);
+console.log("Fecha y hora:", fecha);
