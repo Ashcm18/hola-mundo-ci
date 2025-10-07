f7c384439eccdf5237817c0ae6dd5ea64ba080ab
Ashley Michel Cabrera Mena <amcabreramena@gmail.com>
2025-10-07 15:51:21 -0400

    Probar notificación ntfy.sh

--- hola.js
+++ hola.js
@@ -1,9 +1,1 @@
-// hola.js
-const nombre = "Ash Cm";
-const curso = "Electiva 2 - Integración Continua";
-const fecha = new Date().toLocaleString();
-
 console.log("Hola Mundo");
-console.log("Estudiante:", nombre);
-console.log("Curso:", curso);
-console.log("Fecha y Hora:", fecha);
